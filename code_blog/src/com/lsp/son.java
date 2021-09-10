8ba0e8f174f9a41f17e3eb98a6a1460326872b12
Parth9521 <parth.dhiman@knoldus.com>
2021-09-10 22:05:52 +0530

    first commit

---
+++ code_blog/src/com/lsp/son.java
@@ -0,0 +1,12 @@
+package com.lsp;
+
+class Son extends Father {
+
+    public void reduction(String str) {
+        System.out.println("son : " + str);
+    }
+
+    public void enlarge(CharSequence str){
+        System.out.println("son : " + str);
+    }
+}
